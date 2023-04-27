a99adc83c7f4e3bd2d64fea748cd3701e41d6d7a
B200910002 <jamtsjak123@gmail.com>
2023-04-27 10:10:54 +0800

    .

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,10 +6,16 @@ function Footer() {
     <footer className="bg-light py-3">
       <Container>
         <Row>
-          <Col>
+          <Col sm={8}>
             <p className="text-center">
               Copyright &copy; {new Date().getFullYear()}
             </p>
+          </Col>
+          <Col>
+            <p>Б.Өлзиймаа /B200910009/</p>
+            <p>Б.Жамц /B200910002/</p>
+            <p>Б.Мөнх-Очир /B200910045/</p>
+            <p>Э.Өнөболд /B200910028/</p>
           </Col>
         </Row>
       </Container>
